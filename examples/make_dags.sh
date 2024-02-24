467ffa582cc2cfc643f37068bce17687468bc27a
fgvieira <1151762+fgvieira@users.noreply.github.com>
2024-02-24 11:12:38 +0100

    Code format

--- examples/make_dags.sh
+++ examples/make_dags.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SNAKEMAKE_OPTS="--cores 20 --snakefile ../../workflow/Snakefile --configfile config/config.yaml --use-conda --conda-prefix /tmp/cache/conda $@"
+SNAKEMAKE_OPTS="--snakefile ../../workflow/Snakefile --configfile config/config.yaml --printshellcmds --software-deployment-method conda --conda-prefix /tmp/cache/conda $@"
 
 for TEST in robot_tests robot_tests_big
 do

d154844edfaa7dda6db01de647ef123b7a2959d2
fgvieira <1151762+fgvieira@users.noreply.github.com>
2026-03-06 09:14:41 +0100

    Update tests for HD827sonic.all

--- examples/HD827sonic.all/.tests/unit/test_shard_unicorn.py
+++ examples/HD827sonic.all/.tests/unit/test_shard_unicorn.py
@@ -1,7 +1,6 @@
 """
-Rule test code for unit testing of rules generated with Snakemake 9.12.0.
+Rule test code for unit testing of rules generated with Snakemake 9.16.4.dev3.
 """
-
 
 import os
 import sys
@@ -56,7 +55,8 @@ def test_shard_unicorn(conda_prefix):
 
         # Check the output byte by byte using cmp/zmp/bzcmp/xzcmp.
         # To modify this behavior, you can inherit from common.OutputChecker in here
-        # and overwrite the method `compare_files(generated_file, expected_file), 
+        # and overwrite the method `compare_files(generated_file, expected_file),
         # also see common.py.
         import common
+
         common.OutputChecker(data_path, expected_path, workdir).check()
